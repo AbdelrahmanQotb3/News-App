e632fc3ddccad01838178e31ad2ffd2cdcbbd450
El-Caesar22 <kotpbody5@gmail.com>
2024-10-07 23:42:46 +0300

    Added loadTabDetails function in api manager for handling tabDetails class .

--- lib/Data/api_manager.dart
+++ lib/Data/api_manager.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:news_app/model/articleResponse.dart';
 
 import '../model/sourcesResponse.dart';
 
@@ -19,5 +20,22 @@ abstract class APIManager{
     }catch(e){
       rethrow;
     }
+  }
+
+  static Future<ArticleResponse> loadTabDetails(String sourceID) async{
+    try {
+      Uri url = Uri.parse("https://newsapi.org/v2/everything?apiKey=252ddd1becd44494a69378fa665e0d71&source=${sourceID}");
+      Response apiResponse = await get(url);
+      ArticleResponse articleResponse = ArticleResponse.fromJson(jsonDecode(apiResponse.body));
+      if(apiResponse.statusCode >= 200 && apiResponse.statusCode < 300){
+        return articleResponse;
+      }else{
+        throw "Something went wrong , Please try again later." ;
+      }
+    } catch(e){
+      rethrow ;
+    }
+
+
   }
 }
