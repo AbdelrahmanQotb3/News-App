f65d08ba45730b413732d648df741d42723cdf1c
El-Caesar22 <kotpbody5@gmail.com>
2024-10-08 00:59:41 +0300

    Added Cached Network Image Widget instead of Image.Network.

--- lib/ui/screens/home/tabs/news/tabs_details.dart
+++ lib/ui/screens/home/tabs/news/tabs_details.dart
@@ -1,17 +1,23 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/Data/api_manager.dart';
 import 'package:news_app/model/articleResponse.dart';
 import 'package:news_app/ui/comman%20widgets/app_error.dart';
 import 'package:news_app/ui/comman%20widgets/app_loader.dart';
 
-class TabsDetails extends StatelessWidget {
+class TabsDetails extends StatefulWidget {
   final String sourceID ;
   TabsDetails({super.key , required this.sourceID});
 
   @override
+  State<TabsDetails> createState() => _TabsDetailsState();
+}
+
+class _TabsDetailsState extends State<TabsDetails> {
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: APIManager.loadTabDetails(sourceID),
+        future: APIManager.loadTabDetails(widget.sourceID),
         builder: (context, snapshot){
           if(snapshot.hasError){
             return appError(error: snapshot.error.toString());
@@ -37,13 +43,16 @@ class TabsDetails extends StatelessWidget {
   Widget articleWidget(Article article) {
     return Column(
       children: [
-        Image.network(article.urlToImage ?? ""),
+        CachedNetworkImage(
+          imageUrl: article.urlToImage!,
+          placeholder: (_ , __)=> Center(child: CircularProgressIndicator()),
+          errorWidget: (_ , __ , ___) => Center(child: Icon(Icons.error)),
+
+        ),
         Text(article.source.name ?? ""),
         Text(article.title ?? ""),
         Text(article.publishedAt?? ""),
       ],
     );
   }
-
-
 }
