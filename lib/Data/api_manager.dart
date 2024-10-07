decc28946e529d5f8f021a5f73b1cf3ec28a87c8
El-Caesar22 <kotpbody5@gmail.com>
2024-10-07 17:35:30 +0300

    removed Casting with map variable .

--- lib/Data/api_manager.dart
+++ lib/Data/api_manager.dart
@@ -1,17 +1,17 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
-import 'package:news_app/model/sourceResponse.dart';
+
+import '../model/sourcesResponse.dart';
 
 abstract class APIManager{
-  static Future<SourcesResponse> loadTabsList() async {
+  static Future<SourceResponse> loadTabsList() async {
     try{
       Uri url = Uri.parse("https://newsapi.org/v2/top-headlines/sources?apiKey=252ddd1becd44494a69378fa665e0d71");
       Response response = await get(url);
       // print("Response Body : \n ${response.body}");
-      Map mapBody = jsonDecode(response.body);
+      SourceResponse sourcesResponse = SourceResponse.fromJson(jsonDecode(response.body));
       if(response.statusCode >= 200 && response.statusCode < 300){
-        SourcesResponse sourcesResponse = SourcesResponse.fromJson(mapBody);
         return sourcesResponse;
       }else{
         throw "Something went wrong , Please try again later.";
