ed34183444dbb06e79b39d0246b7ea621c872c3b
El-Caesar22 <kotpbody5@gmail.com>
2024-10-08 19:42:50 +0300

    Created Category Widget For Category tab Screen .

---
+++ lib/ui/screens/home/tabs/category/app_category.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/model/categories.dart';
+
+class AppCategory extends StatelessWidget {
+  final Categories categories;
+  AppCategory({super.key , required this.categories});
+  final radius = Radius.circular(24);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: categories.backgroundColor,
+        borderRadius: BorderRadius.only(
+          topLeft: radius,
+          topRight: radius,
+          bottomLeft: categories.isLeftSided ? Radius.zero : radius,
+          bottomRight: !categories.isLeftSided ? Radius.zero : radius
+        )
+      ),
+      child: Column(
+        children: [
+          Image.asset(categories.imagePath , height: MediaQuery.of(context).size.height *.3,),
+          Text(categories.title , style: TextStyle(color: Colors.white),),
+        ],
+      ),
+    );
+  }
+}
