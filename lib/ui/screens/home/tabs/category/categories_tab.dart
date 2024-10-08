fe305c883216d61d1a80702d6c67b98bec12ff7f
El-Caesar22 <kotpbody5@gmail.com>
2024-10-08 19:41:31 +0300

    Created Categories Tab Screen .

---
+++ lib/ui/screens/home/tabs/category/categories_tab.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/model/categories.dart';
+
+import 'app_category.dart';
+
+class CategoriesTab extends StatelessWidget {
+  static String routeName = "CategoriesTab";
+  const CategoriesTab ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Pick your category of interests ." , style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 14
+        ),),
+        GridView.builder(
+            itemCount: Categories.categories.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemBuilder: (context , index){
+              return AppCategory(categories: Categories.categories[index]);
+            }
+        )
+      ],
+    );
+  }
+}
