6a66a40326c1dbc4fafd63dfe044501dd8e1427b
El-Caesar22 <kotpbody5@gmail.com>
2024-10-08 18:29:31 +0300

    Created Category Class.

---
+++ lib/model/categories.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Categories{
+  String backendId ;
+  String title;
+  String imagePath;
+  bool isLeftSided;
+  Color backgroundColor;
+
+  Categories({
+    required this.backendId,
+    required this.title,
+    required this.imagePath,
+    required this.isLeftSided,
+    required this.backgroundColor
+});
+
+  static List<Categories> categories = [
+    Categories(
+        backendId: "sports",
+        title: "Sports",
+        imagePath: "assets/ball.png",
+        isLeftSided: true,
+        backgroundColor: Colors.red),
+    Categories(
+        backendId: "technology",
+        title: "Technology",
+        imagePath: "assets/Politics.png",
+        isLeftSided: false,
+        backgroundColor: Colors.blue),
+    Categories(
+        backendId: "health",
+        title: "Health",
+        imagePath: "assets/health.png",
+        isLeftSided: true,
+        backgroundColor: Colors.pink),
+    Categories(
+        backendId: "business",
+        title: "Business",
+        imagePath: "assets/business.png",
+        isLeftSided: false,
+        backgroundColor: Colors.brown),
+    Categories(
+        backendId: "entertainment",
+        title: "Entertainment",
+        imagePath: "assets/environment.png",
+        isLeftSided: true,
+        backgroundColor: Colors.blueAccent),
+    Categories(
+        backendId: "science",
+        title: "Science",
+        imagePath: "assets/science.png",
+        isLeftSided: false,
+        backgroundColor: Colors.yellow)
+  ];
+}
