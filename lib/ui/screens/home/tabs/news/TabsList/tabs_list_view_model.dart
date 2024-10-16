faa5e10cd918427673e2c9321629060c6ec35c5e
AbdelrahmanQotb3 <abdelrahmanmahmoud1772@gmail.com>
2024-10-16 17:03:00 +0300

    Added tabsListViewModel file .

---
+++ lib/ui/screens/home/tabs/news/TabsList/tabs_list_view_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/Data/api_manager.dart';
+import 'package:news_app/model/sourcesResponse.dart';
+
+class TabsListViewModel extends ChangeNotifier{
+  TabsListState state = TabsListState.loading ;
+  List<Sources> sources = [];
+  String errorMessage = "" ;
+
+ Future<void> loadTabsList(String categoryID) async {
+   state = TabsListState.loading;
+    try{
+      SourceResponse sourceResponse = await APIManager.loadTabsList(categoryID);
+      state = TabsListState.success;
+      sources = sourceResponse.sources!;
+    }catch(exception){
+      state = TabsListState.error;
+      errorMessage = exception.toString();
+    }
+  }
+}
+enum TabsListState{
+  loading,
+  success,
+  error,
+}
