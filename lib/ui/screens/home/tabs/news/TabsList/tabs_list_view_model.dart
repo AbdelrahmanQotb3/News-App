c2172f0a8a540c92ca15f7e743066a397e8320de
AbdelrahmanQotb3 <abdelrahmanmahmoud1772@gmail.com>
2024-10-16 17:07:55 +0300

    Reformating Code.

--- lib/ui/screens/home/tabs/news/TabsList/tabs_list_view_model.dart
+++ lib/ui/screens/home/tabs/news/TabsList/tabs_list_view_model.dart
@@ -2,24 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:news_app/Data/api_manager.dart';
 import 'package:news_app/model/sourcesResponse.dart';
 
-class TabsListViewModel extends ChangeNotifier{
-  TabsListState state = TabsListState.loading ;
-  List<Sources> sources = [];
-  String errorMessage = "" ;
+class TabsListViewModel extends ChangeNotifier {
+  TabsListState state = TabsListState.loading;
 
- Future<void> loadTabsList(String categoryID) async {
-   state = TabsListState.loading;
-    try{
+  List<Sources> sources = [];
+  String errorMessage = "";
+
+  Future<void> loadTabsList(String categoryID) async {
+    state = TabsListState.loading;
+    try {
       SourceResponse sourceResponse = await APIManager.loadTabsList(categoryID);
       state = TabsListState.success;
       sources = sourceResponse.sources!;
-    }catch(exception){
+    } catch (exception) {
       state = TabsListState.error;
       errorMessage = exception.toString();
     }
   }
 }
-enum TabsListState{
+
+enum TabsListState {
   loading,
   success,
   error,
