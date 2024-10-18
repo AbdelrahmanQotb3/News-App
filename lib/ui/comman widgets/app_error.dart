87369dc60962ba6398e7cf5d6abe1831f64567a6
AbdelrahmanQotb3 <abdelrahmanmahmoud1772@gmail.com>
2024-10-19 01:14:40 +0300

    Added the callBack Function onRefreshClick .

--- lib/ui/comman widgets/app_error.dart
+++ lib/ui/comman widgets/app_error.dart
@@ -2,17 +2,22 @@ import 'package:flutter/material.dart';
 
 class appError extends StatelessWidget {
   String error ;
+  final Function? onRefreshClick;
    appError({
     super.key,
-    required this.error
+    required this.error,
+     this.onRefreshClick
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(error),
-        ElevatedButton(onPressed: (){}, child: Text("Refresh"))
+        ElevatedButton(onPressed: (){
+          onRefreshClick?.call();
+        }, child: Text("Refresh"))
       ],
     );
   }
