e710af1cc20f303b85cb34cf5879f0896d92a129
AbdelrahmanQotb3 <abdelrahmanmahmoud1772@gmail.com>
2024-11-02 15:32:04 +0200

    Calling the NewsRepo Class.

--- lib/ui/screens/home/tabs/news/TabsList/tabs_list_view_model.dart
+++ lib/ui/screens/home/tabs/news/TabsList/tabs_list_view_model.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/Data/api_manager.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:news_app/Data/Repos/news_repo/data_sources/local_data_source/news_local_data_source.dart';
+import 'package:news_app/Data/Repos/news_repo/data_sources/news_repo.dart';
+import 'package:news_app/Data/Repos/news_repo/data_sources/remote_data_sources/news_remote_data_source.dart';
 import 'package:news_app/model/sourcesResponse.dart';
 
 class TabsListViewModel extends ChangeNotifier {
@@ -7,11 +10,12 @@ class TabsListViewModel extends ChangeNotifier {
 
   List<Sources> sources = [];
   String errorMessage = "";
+  NewsRepo newsRepo = NewsRepo(NewsRemoteDateSource(), NewsLocaldataSource(), InternetConnection());
 
   Future<void> loadTabsList(String categoryID) async {
     state = TabsListState.loading;
     try {
-      SourceResponse sourceResponse = await APIManager.loadTabsList(categoryID);
+      SourceResponse sourceResponse = await newsRepo.loadTabsList(categoryID);
       state = TabsListState.success;
       sources = sourceResponse.sources!;
       notifyListeners();
